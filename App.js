63403728c2f069e2d2fa300b6450a33c14479256
Finley Edwards <92788657+Fin0201@users.noreply.github.com>
2024-05-16 00:10:53 +0100

    Implemented registration

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import Activities from './components/Activities';
 import Login from './components/Login';
 import Profile from './components/Profile';
@@ -12,11 +12,54 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Stack = createStackNavigator();
 
-function MainNavigator() {
+export default function App() {
+  //Set variables used when accessing Async Storage
+  const STORAGE_TOKEN = '@token'
+  const STORAGE_USER = '@user'
+  const STORAGE_NAME = '@name'
+
+  //Declare State Variables
+  const [token, setToken] = useState(null)
+  const [loginState, setLoginState] = useState(true)
+  const [returned, setReturned] = useState('')
+
   const [index, setIndex] = useState(0);
+
+  const saveToken = async (result) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_TOKEN, result.token)
+      console.log('Token Set Successfully')
+
+      await AsyncStorage.setItem(STORAGE_USER, result.id)
+      console.log('ID set Successfully')
+
+      await AsyncStorage.setItem(STORAGE_NAME, result.firstName)
+
+      setToken(result.token)
+      return('Login Successfully Validated')
+    }
+    catch (e){
+      return('Failed to Validate Login')
+    }
+  }
+
+  const readToken = async () => {
+    try {
+      const asyncToken = await AsyncStorage.getItem(STORAGE_TOKEN)
+  
+      if(asyncToken !== null){
+        setToken(asyncToken)
+      }
+    }
+    catch (e){
+      console.log('Not Logged In')
+    }
+  }
+  
   const [routes] = useState([
     { key: 'home', title: 'Home', icon: 'home', color: '#6200ee' },
     { key: 'activities', title: 'Activities', icon: 'run', color: '#e91e63' },
@@ -31,29 +74,30 @@ function MainNavigator() {
     profile: Profile,
   });
 
-  return (
-    <BottomNavigation
-      navigationState={{ index, routes }}
-      onIndexChange={setIndex}
-      renderScene={renderScene}
-      shifting={true}
-      barStyle={{ backgroundColor: '#b5e9ff' }}
-      renderIcon={({ route, color }) => (
-        <MaterialCommunityIcons name={route.icon} color={color} size={24} />
-      )}
-    />
-  );
-}
-
-export default function App() {
-  return (
-    <SafeAreaProvider>
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <MainNavigator />
-    </View>
-    </SafeAreaProvider>
-  );
+  if (token === null){
+    return (
+      <Register />
+    )
+  }
+  else {
+    return (
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+            <BottomNavigation
+              navigationState={{ index, routes }}
+              onIndexChange={setIndex}
+              renderScene={renderScene}
+              shifting={true}
+              barStyle={{ backgroundColor: '#b5e9ff' }}
+              renderIcon={({ route, color }) => (
+                <MaterialCommunityIcons name={route.icon} color={color} size={24} />
+              )}
+            />
+        </View>
+      </SafeAreaProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
